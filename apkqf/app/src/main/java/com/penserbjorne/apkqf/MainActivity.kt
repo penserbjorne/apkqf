51c0800db2b5cb60bf0427ee6ccf0cbbaed8e8eb
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2023-02-23 01:14:46 -0600

    Added a small test with a second TextView and a second command

--- apkqf/app/src/main/java/com/penserbjorne/apkqf/MainActivity.kt
+++ apkqf/app/src/main/java/com/penserbjorne/apkqf/MainActivity.kt
@@ -22,28 +22,32 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textViewResults)
         resultTextView.text = execCMD(arrayOf("sh", "-c", "getprop"))
         resultTextView.movementMethod = ScrollingMovementMethod()
+
+        val resultTextView2: TextView = findViewById(R.id.textViewResults2)
+        resultTextView2.text = execCMD(arrayOf("sh", "-c", "service list"))
+        resultTextView2.movementMethod = ScrollingMovementMethod()
     }
-}
 
-fun execCMD (myCMD: Array<String>): String {
-    try {
-        // Creating a process to execute the received command
-        val myProcess = Runtime.getRuntime().exec(myCMD)
+    private fun execCMD (myCMD: Array<String>): String {
+        try {
+            // Creating a process to execute the received command
+            val myProcess = Runtime.getRuntime().exec(myCMD)
 
-        // Reading the result
-        val bufferedReader = BufferedReader(InputStreamReader(myProcess.inputStream))
-        val logger = StringBuilder()
-        var line: String? = ""
+            // Reading the result
+            val bufferedReader = BufferedReader(InputStreamReader(myProcess.inputStream))
+            val logger = StringBuilder()
+            var line: String? = ""
 
-        while (line != null) {
-            line = bufferedReader.readLine()
-            if(line != null) {
-                logger.append(line + "\n")
+            while (line != null) {
+                line = bufferedReader.readLine()
+                if(line != null) {
+                    logger.append(line + "\n")
+                }
             }
+            bufferedReader.close()
+            return logger.toString()
+        } catch (e: IOException) {
+            return e.toString()
         }
-        bufferedReader.close()
-        return logger.toString()
-    } catch (e: IOException) {
-        return e.toString()
     }
 }
